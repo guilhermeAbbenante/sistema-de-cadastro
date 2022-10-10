1ea04b6214292cf62f8be21c6303397354ccbdc8
Guilherme Gomes <guixtx@gmail.com>
2022-10-10 12:17:50 -0300

    voltando para o teste

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 console.log("Testando o git")
-console.log("Enviando para o GitHub")
