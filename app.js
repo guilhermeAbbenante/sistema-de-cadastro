f1a3ae7d9746c1c1c5ed6fb365f24df2e69fe9d9
Guilherme Gomes <guixtx@gmail.com>
2022-10-10 12:08:42 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 console.log("Testando o git")
+console.log("Enviando para o GitHub")
